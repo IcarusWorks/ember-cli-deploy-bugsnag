10b0bf0bf0c7f692cc755ebd6be69cfe7cc9d476
Jordan Morano <jordan.morano@gmail.com>
2017-05-11 15:39:27 -0400

    Initial setup of plugin with testing

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-bugsnag'
+  name: 'ember-cli-deploy-bugsnag',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      requiredConfig: ['apiKey'],
+
+      upload: function(/* context */) {
+        this.log('Uploading sourcemaps to Bugsnag');
+        //do something here to actually deploy your app somewhere
+      },
+    });
+
+    return new DeployPlugin();
+  }
 };
