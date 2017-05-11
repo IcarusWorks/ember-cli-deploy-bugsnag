5cac3f32b1a2ccd7cc2f6f1672c5b31fbf82ad6a
Jordan Morano <jordan.morano@gmail.com>
2017-05-11 17:15:37 -0400

    Update upload hook and first pass on config

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* jshint node: true */
 'use strict';
 
+var RSVP = require('rsvp');
+var fs = require('fs');
+var request = require('request-promise');
+
 var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
@@ -10,11 +14,53 @@ module.exports = {
     var DeployPlugin = BasePlugin.extend({
       name: options.name,
 
-      requiredConfig: ['apiKey'],
+      defaultConfig: {
+        distDir: function(context) {
+          return context.distDir;
+        },
+        distFiles: function(context) {
+          return context.distFiles;
+        },
+        revisionKey: function(context) {
+          return context.revisionData && context.revisionData.revisionKey;
+        },
+      },
 
-      upload: function(/* context */) {
-        this.log('Uploading sourcemaps to Bugsnag');
-        //do something here to actually deploy your app somewhere
+      requiredConfig: ['apiKey', 'baseUrl'],
+
+      upload: function() {
+        var apiKey = this.readConfig('apiKey');
+        var revisionKey = this.readConfig('revisionKey');
+        var distDir = this.readConfig('distDir');
+        var distFiles = this.readConfig('distFiles');
+        var baseUrl = this.readConfig('baseUrl');
+        var promises = [];
+        var jsFiles = distFiles.filter(function(file) {
+          return /assets\/.*\.js$/.test(file);
+        });
+        var mapFiles = distFiles.filter(function(file) {
+          return /assets\/.*\.map$/.test(file);
+        });
+
+        for (var i = 0; i < mapFiles.length; i++) {
+          var mapFile = mapFiles[i];
+          var jsFile = jsFiles[i];
+          var mapFilePath = distDir + '/' + mapFile;
+          var jsFilePath = baseUrl + '/' + jsFile;
+          var formData = {
+            apiKey: apiKey,
+            version: revisionKey,
+            minifiedUrl: jsFilePath,
+            sourceMap: fs.createReadStream(mapFilePath),
+          };
+          var promise = request({
+            uri: 'https://upload.bugsnag.com/',
+            method: 'POST',
+            formData: formData,
+          });
+          promises.push(promise);
+        }
+        return RSVP.all(promises);
       },
     });
 
