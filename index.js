ca498ad6c2df0c89b8ba924cbf5ac73f9dbf9924
Chris Freeman <cfreeman.code@gmail.com>
2018-09-21 12:43:03 -0500

    syntax cleanup

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 
 'use strict';
 
-var RSVP = require('rsvp');
-var fs = require('fs');
-var path = require('path');
-var request = require('request-promise');
-var zlib = require('zlib');
+const RSVP = require('rsvp');
+const fs = require('fs');
+const path = require('path');
+const request = require('request-promise');
+const zlib = require('zlib');
 
-var BasePlugin = require('ember-cli-deploy-plugin');
+const BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
   name: 'ember-cli-deploy-bugsnag',
 
   createDeployPlugin: function(options) {
-    var DeployPlugin = BasePlugin.extend({
+    const DeployPlugin = BasePlugin.extend({
       name: options.name,
 
       defaultConfig: {
@@ -42,51 +42,55 @@ module.exports = {
       requiredConfig: ['apiKey', 'publicUrl'],
 
       upload: function() {
-        var log = this.log.bind(this);
-        var apiKey = this.readConfig('apiKey');
-        var revisionKey = this.readConfig('revisionKey');
-        var distDir = this.readConfig('distDir');
-        var distFiles = this.readConfig('distFiles');
-        var publicUrl = this.readConfig('publicUrl');
-        var overwrite = this.readConfig('overwrite');
-        var includeAppVersion = this.readConfig('includeAppVersion');
-        var promises = [];
-        var jsMapPairs = fetchJSMapPairs(distFiles, publicUrl, distDir);
         log('Uploading sourcemaps to bugsnag', { verbose: true });
 
-        for (var i = 0; i < jsMapPairs.length; i++) {
-          var mapFilePath = jsMapPairs[i].mapFile;
-          var jsFilePath = jsMapPairs[i].jsFile;
-          var formData = {
+        let log = this.log.bind(this);
+        let apiKey = this.readConfig('apiKey');
+        let revisionKey = this.readConfig('revisionKey');
+        let distDir = this.readConfig('distDir');
+        let distFiles = this.readConfig('distFiles');
+        let publicUrl = this.readConfig('publicUrl');
+        let overwrite = this.readConfig('overwrite');
+        let includeAppVersion = this.readConfig('includeAppVersion');
+
+        let jsMapPairs = this._fetchJSMapPairs(distFiles);
+
+        let uploads = jsMapPairs.map(pair => {
+          let mapFilePath = pair.mapFile;
+          let jsFilePath = pair.jsFile;
+          let formData = {
             apiKey: apiKey,
             overwrite: overwrite,
-            minifiedUrl: jsFilePath,
-            sourceMap: this._readSourceMap(mapFilePath)
+            minifiedUrl: publicUrl + jsFilePath,
+            sourceMap: this._readSourceMap(path.join(distDir, mapFilePath))
           };
           if (revisionKey && includeAppVersion) {
             formData.appVersion = revisionKey;
           }
-          var promise = request({
+
+          log('formData', { verbose: true });
+          log(JSON.stringify(formData, { verbose: true }));
+
+          return request({
             uri: 'https://upload.bugsnag.com',
             method: 'POST',
             formData: formData
           });
-          promises.push(promise);
-        }
-        return RSVP.all(promises)
-          .then(function() {
-            log('Finished uploading sourcemaps', { verbose: true });
-          });
+        });
+
+        return RSVP.all(uploads).then(function() {
+          log('Finished uploading sourcemaps', { verbose: true });
+        });
       },
 
       didUpload() {
         this.log('Deleting sourcemaps', { verbose: true });
-        var deleteSourcemaps = this.readConfig('deleteSourcemaps');
+        let deleteSourcemaps = this.readConfig('deleteSourcemaps');
         if (deleteSourcemaps) {
-          var distDir = this.readConfig('distDir');
-          var distFiles = this.readConfig('distFiles');
-          var mapFilePaths = fetchFilePaths(distFiles, distDir, 'map');
-          var promises = mapFilePaths.map(function(mapFilePath) {
+          let distDir = this.readConfig('distDir');
+          let distFiles = this.readConfig('distFiles');
+          let mapFilePaths = fetchFilePathsByType(distFiles, distDir, 'map');
+          let promises = mapFilePaths.map(function(mapFilePath) {
             return new RSVP.Promise(function(resolve, reject) {
               fs.unlink(mapFilePath, function(err) {
                 if (err) {
