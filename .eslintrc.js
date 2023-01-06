5fe3a01d698d3bc892fafe23c7b6eab6d5333160
Philip Dudley <435890+pdud@users.noreply.github.com>
2023-01-06 10:18:11 -0500

    chore: add github actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,69 @@
+'use strict';
+
 module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: "module"
+    sourceType: 'module'
   },
-  plugins: ["ember", "prettier"],
-  extends: ["eslint:recommended", "plugin:ember/recommended", "prettier"],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
   env: {
     browser: true
   },
   rules: {
-    "ember/avoid-leaking-state-in-ember-objects": 0,
-    "prettier/prettier": ["error", { singleQuote: true }]
+    'ember/avoid-leaking-state-in-ember-objects': 0,
+    'prettier/prettier': ['error', { singleQuote: true }]
   },
   overrides: [
     // node files
     {
       files: [
-        ".template-lintrc.js",
-        "ember-cli-build.js",
-        "index.js",
-        "testem.js",
-        "blueprints/*/index.js",
-        "config/**/*.js",
-        "tests/dummy/config/**/*.js"
+        '.eslintrc.js',
+        '.prettierrc.js',
+        './tests/.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
       ],
       excludedFiles: [
-        "addon/**",
-        "addon-test-support/**",
-        "app/**",
-        "tests/dummy/app/**"
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
         ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
       },
-      plugins: ["node"],
+      plugins: ['node'],
       rules: Object.assign(
         {},
-        require("eslint-plugin-node").configs.recommended.rules,
+        require('eslint-plugin-node').configs.recommended.rules,
         {
           // add your custom rules and overrides for node files here
         }
       )
     },
     {
-      files: ["tests/unit/**/*.js"],
+      files: ['tests/unit/**/*.js'],
       env: {
         browser: false,
         node: true,
         mocha: true
       },
-      plugins: ["node"],
+      plugins: ['node'],
       rules: Object.assign(
         {},
-        require("eslint-plugin-node").configs.recommended.rules,
+        require('eslint-plugin-node').configs.recommended.rules,
         {
           // add your custom rules and overrides for node files here
         }
